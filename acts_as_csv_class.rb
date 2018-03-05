36592390e6d1e9f79a93b32311a1afed201507a6
Kevin Moore <k.james.moore@gmail.com>
2018-03-05 16:29:28 -0500

    Domain Specific Language

---
+++ acts_as_csv_class.rb
@@ -0,0 +1,39 @@
+# design ur own Domain Specific Language
+
+#
+# as a superclass
+#
+# class ActsAsCsv
+#     def read
+#         file = File.new(self.class.to_s.downcase + '.txt')
+#         @headers = file.gets.chomp.split(', ')
+#
+#         file.each do |row|
+#             @result << row.chomp.split(", ")
+#         end
+#     end
+#
+#     def headers
+#         @headers
+#     end
+#
+#     def csv_contents
+#         @result
+#     end
+#
+#     def initialize
+#         @result = []
+#         read
+#     end
+# end
+#
+# class RubyCsv < ActsAsCsv
+# end
+
+#
+# as a macro
+#
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
